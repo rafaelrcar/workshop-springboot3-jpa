2c3a2a0457c9b190d0c5d1fbe2dbbe6c713e0669
Rafael Ribeiro Carvalho <rafaelcar203@gmail.com>
2023-11-22 00:02:57 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/service/resources/UserResource.java
+++ src/main/java/com/example/service/resources/UserResource.java
@@ -14,6 +14,7 @@ public class UserResource {
 	@GetMapping
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "Milena", "milena@gmail.com", "99999", "12345");
+		
 		return ResponseEntity.ok().body(u);
 	}
 }
